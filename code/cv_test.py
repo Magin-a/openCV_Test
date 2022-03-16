664aaa2f9bf2d5a1ee5ed0b4464b067f2c2d46bc
Magin-a <80820166+Magin-a@users.noreply.github.com>
2022-03-16 11:07:06 +0900

    Update cv_test.py

--- code/cv_test.py
+++ code/cv_test.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 
 #불러오기
-img = cv.imread("openCV_Test\IMAGE\lulu.jpg")  #ex) cv.imread(r'C:\Users\starry_night.jpg')
+img = cv.imread("파일 경로")  #ex) cv.imread(r'C:\Users\starry_night.jpg')
 cv.imshow('img', img)
 cv.waitKey(0)
 cv.destroyAllWindows()
